bf322be71c69b5e750781a9448de1f80d1a040d2
Matt <cuffaro.matt@gmail.com>
2024-01-16 15:25:10 -0500

    fixed init.sh

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DEFAULT_REPO='AlgebraicX.jl'
+DEFAULT_REPO='AlgebraicX'
 DEFAULT_UUID='b66562e1-fa90-4e8b-9505-c909188fab76' 
 
 usage="This script is for initializing the template with the new repository name and UUID. Please provide the new repository name and UUID in that order.
@@ -32,21 +32,26 @@ then
 fi
 
 # rename 
-mv src/$DEFAULT_REPO.jl src/$REPO.jl
+if [[ $REPO == *.jl ]]
+then
+  mv src/$DEFAULT_REPO.jl src/$REPO
+else
+  mv src/$DEFAULT_REPO.jl src/$REPO.jl
+fi
 
 # get version
 unameOut="$(uname -s)"
 
 case "${unameOut}" in
   Linux*)  
-    git grep -l $DEFAULT_REPO | xargs sed -i 's/{$REPO}/{$REPO}/g';
-    git grep -l $DEFAULT_UUID | xargs sed -i 's/{$UUID}/{$UUID}/g';;
+    git grep -l $DEFAULT_REPO | xargs sed -i 's/{$DEFAULT_REPO}/{$REPO}/g';
+    git grep -l $DEFAULT_UUID | xargs sed -i 's/{$DEFAULT_UUID}/{$UUID}/g';;
   Darwin*) 
-    git grep -l $DEFAULT_REPO | xargs sed -i '' -e 's/{$REPO}/{$REPO}/g';
-    git grep -l $DEFAULT_UUID | xargs sed -i '' -e 's/{$UUID}/{$UUID}/g';;
+    git grep -l $DEFAULT_REPO | xargs sed -i '' -e 's/{$DEFAULT_REPO}/{$REPO}/g';
+    git grep -l $DEFAULT_UUID | xargs sed -i '' -e 's/{$DEFAULT_UUID}/{$UUID}/g';;
   *)       
     echo UNKNOWN:${unameOut};; 
 esac
 
-git commit -am "Initialized $REPO"
-git push
+# git commit -am "Initialized $REPO"
+# git push
