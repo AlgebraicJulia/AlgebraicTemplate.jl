574fec16dcb09597fd29fe3ce86be986b8c65b60
Matt <cuffaro.matt@gmail.com>
2024-02-02 21:22:25 -0500

    init.sh now gets current folder name

--- .buildkite/jobscript.sh
+++ .buildkite/jobscript.sh
@@ -10,7 +10,7 @@ pwd; hostname; date
 module load julia
 
 echo "Running Tests..."
-julia --project -t 16 -e 'using Pkg; Pkg.status(); Pkg.test()'
+julia --project -t 32 -e 'using Pkg; Pkg.status(); Pkg.test()'
 
 echo "Building Documentation..."
-julia --project=docs -t 16 -e'using Pkg; Pkg.develop(PackageSpec(path=pwd())); Pkg.status(); Pkg.instantiate(); include("docs/make.jl")'
+julia --project=docs -t 32 -e'using Pkg; Pkg.develop(PackageSpec(path=pwd())); Pkg.status(); Pkg.instantiate(); include("docs/make.jl")'
